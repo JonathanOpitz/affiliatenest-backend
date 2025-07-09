2af4f5b46247cfc29dafcfd1d4a8a6bb766fa5c3
JonathanKikker <jonathan@kikker.nl>
2025-07-09 21:41:45 +0200

    Remove deprecated MongoDB driver options

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected to', conn.connection.db.namespace);
   } catch (error) {
     console.error('MongoDB connection error:', error.message, error.stack);
